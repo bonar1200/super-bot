f7cf1655ac60dcac1083d4f0413953de78e65f70
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-06-09 07:13:44 +0200

    Create bot1.js

---
+++ bot1.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+	      var interval = setInterval (function () {
+client.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
+		  }, 120000);
+
+});
+client.login(process.env.BOT_TOKENN);
