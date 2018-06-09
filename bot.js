36bcf535c5c5dfd509650d4e036929f65a79a62e
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-06-09 05:46:21 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+	      var interval = setInterval (function () {
+client.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
+		  }, 120000);
+
+});
+client.login(process.env"mfa._jAdOp0aLYAPStYqmlb4EkWfPJw0cYyG-KhCeMocKvXxyNj647aNXjK_GD7nsBCcrRI7c0CjT-vF43buPstJ");
