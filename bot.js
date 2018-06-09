48426d5947aff5d4e1dc46b739dea8dadf9aaae5
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-06-09 07:56:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,11 @@ client.on("ready", () => {
 	      var interval = setInterval (function () {
 client.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
 		  }, 120000);
-
+  	      var interval1 = setInterval (function () {
+client.channels.get('451866194559893504').send('t!daily <@448675703018487819>');
+  client.channels.get('451866194559893504').send('#daily');
+client.channels.get('451866194559893504').send('t!rep <@448675703018487819>');
+  client.channels.get('451866194559893504').send('#rep <@448675703018487819>');  
+		  }, 86800000);
 });
 client.login(process.env.BOT_TOKEN);
