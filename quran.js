7879f6352fa85668a8d27a5ef98bb9eff43a15a8
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-06-12 06:23:41 +0200

    Update quran.js

--- quran.js
+++ quran.js
@@ -1,35 +1,34 @@
 const Discord = require("discord.js");
-const prefix = '!';
-const client = new Discord.Client();
+
+const bot = new Discord.Client();
+
 client.on('message', message => {
-if (message.content.startsWith(prefix + 'help')) {
-message.reply(`**سوف يتم ارسال قائمة المساعدة في رسالة خاصة **`); {
+
+if(message.content === "!قران")
+  return message.reply("**أوامر بوت القرآن:\nقران1: القرآن الكريم كامل بوت الشيخ عبدالباسط عبدالصمد\nقران2: القرآن الكريم كامل بصوت الشيخ ماهر المعيقلي**")
+
+client.on('message', message => {
+  if (message.content.startsWith('قران1')) {
+    const voiceChannel = message.member.voiceChannel;
+    voiceChannel.join()
+      .then(connnection => {
+        const stream = ytdl("https://www.youtube.com/watch?v=vqXLGtZcUm8", { filter: 'audioonly' });
+        const dispatcher = connnection.playStream(stream);
+                dispatcher.on('end', () => voiceChannel.leave());
+
+      });
+  }
   
-              var embed  = new Discord.RichEmbed()
-                .addField("**!giverole**" ,"**الاستخدام:** ``لاعطاء شحص رتبة``")
-                .addField("**!removerole**", "**الاستخدام :** ``مسح رتبة من شخص معين``")
-                .addField("**!deleterole**" ,"**الاستخدام:** ``مسح رتبة من السيرفر ``")
-                .addField("**!rolebc**" ,"**الاستخدام:** ``برودكاست لأشخاص لهم رتبة معينة``")
-                .addField("**!bc**" ,"**الاستخدام:** ``برودكاست لجميع السيرفر``")
-                .addField("**!deletechannel**" ,"**الاستخدام:** ``مسح روم من السيرفر``")
-                .addField("**!addtext**" ,"**الاستخدام:** ``اضافة روم كتابي``")
-                .addField("**!addvoice**" ,"**الأستخدام:** ``اضافة روم صوتي``")
-                .addField("**قفل الروم**" ,"**الاستخدام:** ``لقفل الروم وعدم الكتابة به``")
-                .addField("**فتح الروم**" ,"**الاستخدام:** ``لفتح الروم مرة أخرى``")
-                .addField("**تنظيف**" ,"**الاستخدام:** ``لمسح الرسائل``")
-                .addField("**!createrole**" ,"**الاستخدام:** ``لعمل رتبة``  ")
-                .addField("**!vb**" ,"**الاستخدام:** ``باند من الرومات الصوتية``")
-                .addField("**!unvb**", "**الأستخدام:** ``فك الباند من الرومات الصوتية``")
-                .addField("**!ban**", "**الاستخدام** ``لتبنيد شخص``")
-                .addField("**!unban**", "**الاستخدام** ``لفك باند شخص``")
-                .addField("**!kick**", "**الاستخدام** ``لطرد شخص``")
-                .addField("**اسكت**", "**الأستخدام:** ``للميوت الكتابي``")
-                .addField("**تكلم**", "**الاستخدام** ``لفك الميوت الكتابي``")
-              .setColor('RANDOM')
-.setColor('RANDOM')
-  message.author.sendEmbed(embed);
-}
-    }
+  if (message.content.startsWith('قران2')) {
+    const voiceChannel = message.member.voiceChannel;
+    voiceChannel.join()
+      .then(connnection => {
+        const stream = ytdl("https://www.youtube.com/watch?v=wwMyn8a_puQ", { filter: 'audioonly' });
+        const dispatcher = connnection.playStream(stream);
+                dispatcher.on('end', () => voiceChannel.leave());
+    });
+            }
 });
 
-client.login(process.env.BOT_QURAN);
+});
+client.login('NDU1NTkzNjE2MzY3Mjg4MzQw.Df-QNw.D3hadPBOR0zr6oaHCLlcEbQGCC8');
