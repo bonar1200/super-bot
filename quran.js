5a4915f262b85e5286e7f268bede33d826bf85a8
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-06-13 07:35:42 +0200

    Update quran.js

--- quran.js
+++ quran.js
@@ -9,17 +9,20 @@ client.on('message', message => {
 if(message.content === "!قران")
   return message.reply("**أوامر بوت القرآن:\nقران1: القرآن الكريم كامل بوت الشيخ عبدالباسط عبدالصمد\nقران2: القرآن الكريم كامل بصوت الشيخ ماهر المعيقلي**")
 
-client.on('message', message => {
   if (message.content.startsWith('قران1')) {
-    const voiceChannel = message.member.voiceChannel;
-    voiceChannel.join()
-      .then(connnection => {
-        const stream = ytdl("https://www.youtube.com/watch?v=vqXLGtZcUm8", { filter: 'audioonly' });
-        const dispatcher = connnection.playStream(stream);
-                dispatcher.on('end', () => voiceChannel.leave());
+        message.react('🔊')}  return;
+    const stram0ptions = { seek: 0, volume: 100};  
+    const broadcast = 
+clien.createVoiceBroadcast();
 
-      });
-  }
+
+
+message.gulid.member(message.author).voiceChannel.join()
+       .then(connection => {
+           const stream = ytdl('https://www.youtube.com/watch?v=Ktync4j_nmA', { filter : 'audioonly'});
+           broadcast.playstream(stream);
+           const dispatcher = connection.playBroadcast(broadcast);
+ }).catch(console.error);
   
   if (message.content.startsWith('قران2')) {
     const voiceChannel = message.member.voiceChannel;
