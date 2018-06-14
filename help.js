0f99442ed2def0713335ae1501cc29af31a9caea
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-06-15 00:56:08 +0200

    Update help.js

--- help.js
+++ help.js
@@ -23,7 +23,7 @@ client.on("ready", () => {
 const queue = new Map();
 client.on('message', message => {
 if (message.content === prefix + 'help admin') {
-        if (!message.member.hasPermission('MOVE_MEMBERS') || !message.member.hasPermission('ADMINISTRATOR') || !message.member.hasPermission('MANAGA_ROLES') || !message.member.hasPermission('MANAGE_MESSAGES') || !message.member.hasPermission('MANAGE_CHANNELS') || !message.member.hasPermission('MANAGE_NICKNAMES') || !message.member.hasPermission('BAN_MEMBERS') || !message.member.hasPermission('KICK_MEMBERS') || !message.member.hasPermission('DEAFEAN_MEMBERS') || !message.member.hasPermission('MUTE_MEMBERS') || !message.member.hasPermission('MANAGE_EMOJIS')) {
+        if (!message.member.hasPermission('MOVE_MEMBERS') || !message.member.hasPermission('ADMINISTRATOR') || !message.member.hasPermission('MANAGA_ROLES') || !message.member.hasPermission('MANAGE_MESSAGES') || !message.member.hasPermission('MANAGE_CHANNELS') || !message.member.hasPermission('MANAGE_NICKNAMES') || !message.member.hasPermission('BAN_MEMBERS') || !message.member.hasPermission('KICK_MEMBERS') || !message.member.hasPermission('DEAFEN_MEMBERS') || !message.member.hasPermission('MUTE_MEMBERS') || !message.member.hasPermission('MANAGE_EMOJIS')) {
             return message.reply("** عفوًا, لايمكنك أستخدام هذا البوت لإنك لست من إدارة السيرفر **");
         }
 message.reply(`**سوف يتم ارسال قائمة المساعدة في رسالة خاصة **`); {
