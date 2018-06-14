f40a989a469e3ad20fa369cb8e8feb22a29abd5d
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-06-15 01:56:56 +0200

    Update move.js

--- move.js
+++ move.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const moment = require('moment');
 const ytdl = require('ytdl-core');
-const prefix = 'r!';
+const prefix = '!!';
 const YouTube = require('simple-youtube-api');
 const ms = require("ms");
 
@@ -14,7 +14,7 @@ if(message.content.startsWith(prefix + 'move')) {
 
  if (message.member.hasPermission("MOVE_MEMBERS")) {
  if (message.mentions.users.size === 0) {
- return message.channel.send("**لاستخدم الأمر اكتب : ' +prefix+ 'move @user**")
+ return message.channel.send("**لاستخدم الأمر اكتب : !move @user**")
 }
 if (message.member.voiceChannel != null) {
  if (message.mentions.members.first().voiceChannel != null) {
