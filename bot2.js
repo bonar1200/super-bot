d32cbf86a13bb8bd25b1d5b8a8adcbcf2de6ce01
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-06-14 06:44:09 +0200

    Create bot2.js

---
+++ bot2.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+
+	      var interval = setInterval (function () {
+client.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
+		  }, 120000);
+  
+	      var interval1 = setInterval (function () {
+  client.channels.get('451866194559893504').send('#daily');
+  client.channels.get('451866194559893504').send('#rep <@215251553538408458>');  
+		  }, 86800000);
+  
+	      var interval2 = setInterval (function () {
+client.channels.get('451866194559893504').send('t!daily <@215251553538408458>');
+client.channels.get('451866194559893504').send('t!rep <@215251553538408458>');
+		  }, 61360000);
+});
+client.login(process.env.BOT_2);
