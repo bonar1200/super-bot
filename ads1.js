490d08543959b07b783ab7c1dbb3295914f9fbbb
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-07-03 11:20:57 +0200

    Update ads1.js

--- ads1.js
+++ ads1.js
@@ -1,22 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on("ready", () => {
 
-          var interval = setInterval (function () {
-client.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
-          }, 120000);
-  
-          var interval1 = setInterval (function () {
-  client.channels.get('451866194559893504').send('#daily');
-  client.channels.get('451866194559893504').send('#rep <@215251553538408458>');  
-          }, 86800000);
-  
-          var interval2 = setInterval (function () {
-client.channels.get('451866194559893504').send('t!daily <@215251553538408458>');
-client.channels.get('451866194559893504').send('t!rep <@215251553538408458>');
-          }, 61360000);
-});
 
 client.on("guildMemberAdd", member => {
   member.createDM().then(function (author) {
@@ -27,4 +12,4 @@ client.on("guildMemberAdd", member => {
 
 
 
-client.login(process.env.ADS_1);
+client.login(process.env.AD_1);
