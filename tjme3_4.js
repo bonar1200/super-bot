8f7ebbfd665548e00356fb5a51e1119c731a8da4
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-07-03 23:52:05 +0200

    Update and rename bot5.js to tjme3_4.js

--- tjme3_4.js
+++ tjme3_4.js
@@ -2,21 +2,29 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-client.channels.get('451866194559893504').send('t!credit <@448675703018487819> 430000');
 
-
-	      var interval = setInterval (function () {
+          var interval = setInterval (function () {
 client.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
-		  }, 120000);
+          }, 120000);
   
-	      var interval1 = setInterval (function () {
+          var interval1 = setInterval (function () {
   client.channels.get('451866194559893504').send('#daily');
   client.channels.get('451866194559893504').send('#rep <@215251553538408458>');  
-		  }, 86800000);
+          }, 86800000);
   
-	      var interval2 = setInterval (function () {
+          var interval2 = setInterval (function () {
 client.channels.get('451866194559893504').send('t!daily <@215251553538408458>');
 client.channels.get('451866194559893504').send('t!rep <@215251553538408458>');
-		  }, 61360000);
+          }, 61360000);
 });
-client.login(process.env.BOT_5);
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (author) {
+  return author.send('** تنورنا في سيرفرنا يا عسل 💖 \n https://discord.gg/3AWMGEA**')
+
+}).catch(console.error)
+})
+
+
+
+client.login(process.env.TJME3_4);
