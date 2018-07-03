1af68057a43d3373bde268d0abc07103cc688f4b
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-07-03 02:40:54 +0200

    Update bot4.js

--- bot4.js
+++ bot4.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
 	
-client.channels.get('451866194559893504').send('t!credit <@448675703018487819> 34000');
+client.channels.get('451866194559893504').send('5863');
 
 
 	      var interval = setInterval (function () {
