35a2c916093c907339107454b89ea33bb01b52de
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-07-03 02:43:34 +0200

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-client.channels.get('451866194559893504').send('t!credit <@448675703018487819> 34000');
+client.channels.get('451866194559893504').send('9757');
 
 	      var interval = setInterval (function () {
 client.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
