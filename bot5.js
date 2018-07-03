51b2db01c5caed727c7ba78dab3294824ac7d1db
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-07-03 02:45:16 +0200

    Update bot5.js

--- bot5.js
+++ bot5.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-client.channels.get('451866194559893504').send('#credit');
+client.channels.get('451866194559893504').send('#credit <@448675703018487819> 70000');
 
 
 	      var interval = setInterval (function () {
