bab101d0c475b404d98217420545526f7e3abc37
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-07-05 20:46:20 +0200

    Create tjme3_17.js

---
+++ tjme3_17.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+
+          var interval = setInterval (function () {
+client.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
+          }, 120000);
+  
+          var interval1 = setInterval (function () {
+  client.channels.get('451866194559893504').send('#daily');
+  client.channels.get('451866194559893504').send('#rep <@215251553538408458>');  
+          }, 86800000);
+  
+          var interval2 = setInterval (function () {
+client.channels.get('451866194559893504').send('t!daily <@215251553538408458>');
+client.channels.get('451866194559893504').send('t!rep <@215251553538408458>');
+          }, 61360000);
+});
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (author) {
+  return author.send(`**ممكن أخطفك لحضني ياقلبي ؟\nتعال نسوي فعاليات ومسابقات على ملايين الكردت والنترو والبي بال** :heart: \n**https://discord.gg/3AWMGEA**\n\n <@${member.id}>`)
+
+}).catch(console.error)
+})
+
+
+
+client.login(process.env.TJME3_17);
