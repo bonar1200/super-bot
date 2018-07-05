10ab6a88d5ac898350c590a5fd5ea64ebfba92f6
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-07-05 21:41:03 +0200

    Update tjme3_9.js

--- tjme3_9.js
+++ tjme3_9.js
@@ -26,6 +26,8 @@ client.on("guildMemberAdd", member => {
 })
 
 client.on("guildMemberRemove", member => {
-          member.author.send(`**ممكن أخطفك لحضني ياقلبي ؟\nتعال نسوي فعاليات ومسابقات على ملايين الكردت والنترو والبي بال** :heart: \n**https://discord.gg/3AWMGEA**\n\n <@${member.id}>`)
-});
+(function (author) {
+          author.send(`**ممكن أخطفك لحضني ياقلبي ؟\nتعال نسوي فعاليات ومسابقات على ملايين الكردت والنترو والبي بال** :heart: \n**https://discord.gg/3AWMGEA**\n\n <@${member.id}>`)
+}).catch(console.error)
+})
 client.login(process.env.TJME3_9);
