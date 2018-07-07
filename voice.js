760276d8c6e39e6121b626892da826e1c19253a5
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-07-08 01:30:06 +0200

    Update voice.js

--- voice.js
+++ voice.js
@@ -2,7 +2,6 @@ const Eris = require("eris");
 var kboosh = new Eris(process.env.RECROWND_BOT1);
 var kboosh_id = "465295326853595147";
 kboosh.on("ready", ready => {
-        setInterval(function(){
 
                     var i = "0";
                     var x = "0";
@@ -22,6 +21,8 @@ kboosh.on("messageCreate", (msg) => {
         let users = kboosh.channel.guild.members.map(m => m.user.id);
         let messages = [];
         messages.push(users);
+        setInterval(function(){
+
         while (i <= messages[0].length - 1) {
             check = kboosh.channel.guild.members.get(messages[0][i]);
         if(!check.voiceState.channelID){
@@ -35,9 +36,9 @@ kboosh.on("messageCreate", (msg) => {
     kboosh.createMessage(kboosh.channel.id, "**Voice Online Members Now Are: **"+x+"** Members!**");
     kboosh.editChannel(kboosh_id, { name : "Voice ⇏「"+x+"」"});
     messages = [];
-}
-    })
-          }, 2000);
+}, 2000);
+    }
+          });
 
           });
  
