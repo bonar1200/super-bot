ae15699d14694a81f9450f01317695715d345cc3
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-08-02 17:14:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
 
-	//
 	      var interval = setInterval (function () {
 client.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
 		  }, 120000);
