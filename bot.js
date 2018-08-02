3259218692cfba437d04283d6a5e8f6a7728e1fa
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-08-02 16:52:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-	     client.user.setActivity(`Your Ass`,{type: 'WATCHING'},"https://www.twitch.tv/scarlxrd")
 
 	
 	      var interval = setInterval (function () {
