376f62f7107598c1b3df905f40b32e06cf1daa43
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-08-02 17:09:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
+client.user.setStatus('online');
 
 	//
 	      var interval = setInterval (function () {
