0b5247055b633368929d045b4dcb5ea64d9d8934
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-08-07 02:28:36 +0200

    Update stark_music.js

--- stark_music.js
+++ stark_music.js
@@ -69,7 +69,7 @@ const embed = new Discord.RichEmbed()
   .setAuthor("Stark Music", "http://memberdata.s3.amazonaws.com/hi/hitsdd/photos/hitsdd_photo_gal__photo_1923946100.png")
 
   .setColor('RANDOM')
-  .setDescription(`اختر رقم المقطع : \n${videos.map(video2 => `**[${++index}] -** ${video2.title}``).join('\n'`)}`)
+  .setDescription(`اختر رقم المقطع : \n${videos.map(video2 => `**[${++index}] -** ${video2.title}`).join('\n')}`)
 
 
 
