4828f7a8ce8e414a16b5c1a440943af2144bb0e6
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-08-07 18:04:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,4 +17,24 @@ client.channels.get('451866194559893504').send('t!rep <@448675703018487819>');
 		  }, 61360000);
 });
 client.login(process.env.BOT_TOKEN);
+
+// STARK BANK
+const client1 = new Discord.Client();
+
+client1.on("ready", () => {
+
+	      var interval = setInterval (function () {
+client1.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
+		  }, 120000);
+  
+  	      var interval1 = setInterval (function () {
+  client1.channels.get('451866194559893504').send('#daily');
+  client1.channels.get('451866194559893504').send('#rep <@448675703018487819>');  
+		  }, 86800000);
+	      var interval2 = setInterval (function () {
+client1.channels.get('451866194559893504').send('t!daily <@448675703018487819>');
+client1.channels.get('451866194559893504').send('t!rep <@448675703018487819>');
+		  }, 61360000);
+});
+client1.login(process.env.STARK_BANG);
 // //
