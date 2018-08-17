be9babd808e49b347e49a69a6f6262818a85b8b2
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-08-17 15:55:50 +0200

    Update tag1.js

--- tag1.js
+++ tag1.js
@@ -1,8 +1,8 @@
 const myDiscriminator = ["6666", "2121", "0000", "0001", "0002", "6969", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "2169", "6921", "0021", "0690", "0210", "2100", "0069"];
 const bot = new(require("discord.js")).Client({fetchAllMembers: true});
 
-const password = "";
-const token = "";
+const password = "xixi6060";
+const token = "NDc5OTcwMTU3ODEzNTYzNDEz.Dlhe1Q.r50SgVLNTjHQKcKxHGX_alNbYqk";
 
 bot.login(token);
 
@@ -11,15 +11,16 @@ function changeDiscriminator() {
     return console.log("Discriminator Loaded: " + bot.user.discriminator);
   try {
     const us = bot.users.find(u => u.discriminator === bot.user.discriminator && u.username !== bot.user.username && !u.bot).username;
+    console.log(Date.now(), "Username Loaded: " + us);
     bot.user.setUsername(us, password).then((u) => {
+      console.log(Date.now(), "Username: " + u.username, "Discriminator: " + u.discriminator);
       setTimeout(changeDiscriminator, 8640 * 10000);
-    }).catch(err)
-  } catch(e) {
-    setTimeout(changeDiscriminator, 30 * 1e3);
+    })
   }
 }
 
 bot.once("ready", () => {
+  console.log(Date.now(), "Started with " + bot.users.size + " users.");
   changeDiscriminator();
   if(myDiscriminator.includes(bot.user.discriminator)) {
       console.log(`I successfully got the discrim ${bot.user.discriminator}!`) 
