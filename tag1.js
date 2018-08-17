ed406068c3274e528a4db22ce97f8a17dabea7fd
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-08-17 03:27:06 +0200

    Update tag1.js

--- tag1.js
+++ tag1.js
@@ -1,8 +1,8 @@
 const myDiscriminator = ["6666", "2121", "0000", "0001", "0002", "6969", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "2169", "6921", "0021", "0690", "0210", "2100", "0069"];
 const bot = new(require("discord.js")).Client({fetchAllMembers: true});
 
-const password = "xixi6060";
-const token = "NDc5NzE4NDczNzMyNDU2NDcz.DleXaA.C2u3j2X65xSEj0jhsErmhMCNRmQ";
+const password = "";
+const token = "";
 
 bot.login(token);
 
