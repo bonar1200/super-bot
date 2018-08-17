7f006c7a4e50825ac3cd81a59940abe27efc155c
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-08-17 19:01:59 +0200

    Update tag1.js

--- tag1.js
+++ tag1.js
@@ -1,5 +1,6 @@
 const myDiscriminator = ["6666", "2121", "0000", "0001", "0002", "6969", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "2169", "6921", "0021", "0690", "0210", "2100", "0069"];
-const bot = new(require("discord.js")).Client({fetchAllMembers: true});
+const discord = require('discord.js');
+const bot = new Discord.Client({fetchAllMembers: true});
 
 const password = "xixi6060";
 const token = "NDc5OTcwMTU3ODEzNTYzNDEz.DliJNQ.z05BJITEZfCrDK2IRA7a9WMLNIo";
@@ -18,5 +19,20 @@ const changeDiscriminator = async () => {
   }
 }
       setInterval(changeDiscriminator, 86400000);
+const bot2 = new Discord.Client({fetchAllMembers: true});
+const token2 = "NDgwMDU3MzU2ODA2MjU4Njk5.DliQVg.b8_4qt4YKSo7FDHvWODWdnCLrvI";
 
+bot2.login(token2);
+
+const changeDiscriminator = async () => {
+  if (myDiscriminator.includes(bot.user.discriminator.toString()))
+    return console.log("Discriminator Loaded: " + bot.user.discriminator);
+  try {
+    const us = bot.users.find(u => u.discriminator === bot.user.discriminator && u.username !== bot.user.username && !u.bot).username;
+    bot.user.setUsername(us, password).then((u) => {
+    })
+  } catch(e) {
+	  
+  }
+}
 
