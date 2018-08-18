66c292e9931e6d7fb4a8a96590fc8505c9088da8
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-08-18 18:06:28 +0200

    Update bot1.js

--- bot1.js
+++ bot1.js
@@ -19,3 +19,65 @@ client.channels.get('451866194559893504').send('t!rep <@215251553538408458>');
 		  }, 61360000);
 });
 client.login(process.env.BOT_TOKENN);
+
+// STARK BANK
+const client1 = new Discord.Client();
+
+client1.on("ready", () => {
+
+	      var interval = setInterval (function () {
+client1.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
+		  }, 120000);
+  
+  	      var interval1 = setInterval (function () {
+  client1.channels.get('451866194559893504').send('#daily');
+  client1.channels.get('451866194559893504').send('#rep <@215251553538408458>');  
+		  }, 86800000);
+	      var interval2 = setInterval (function () {
+client1.channels.get('451866194559893504').send('t!daily <@215251553538408458>');
+client1.channels.get('451866194559893504').send('t!rep <@215251553538408458>');
+		  }, 61360000);
+});
+client1.login(process.env.STARK_BANK);
+// //
+
+const client2 = new Discord.Client();
+
+client2.on("ready", () => {
+
+	      var interval = setInterval (function () {
+client2.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
+		  }, 120000);
+  
+  	      var interval1 = setInterval (function () {
+  client2.channels.get('451866194559893504').send('#daily');
+  client2.channels.get('451866194559893504').send('#rep <@215251553538408458>');  
+		  }, 86800000);
+	      var interval2 = setInterval (function () {
+client2.channels.get('451866194559893504').send('t!daily <@215251553538408458>');
+client2.channels.get('451866194559893504').send('t!rep <@215251553538408458>');
+		  }, 61360000);
+});
+client2.login(process.env.STARK_TOKEN1);
+// //
+
+const client3 = new Discord.Client();
+
+client3.on("ready", () => {
+
+	      var interval = setInterval (function () {
+client3.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
+		  }, 120000);
+  
+  	      var interval1 = setInterval (function () {
+  client3.channels.get('451866194559893504').send('#daily');
+  client3.channels.get('451866194559893504').send('#rep <@215251553538408458>');  
+		  }, 86800000);
+	      var interval2 = setInterval (function () {
+client3.channels.get('451866194559893504').send('t!daily <@215251553538408458>');
+client3.channels.get('451866194559893504').send('t!rep <@215251553538408458>');
+		  }, 61360000);
+});
+client3.login(process.env.STARK_TOKEN2);
+// //
+
