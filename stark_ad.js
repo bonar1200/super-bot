8069efa2b619e1d4366d5cabdddfc974b4e30609
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-08-19 19:04:48 +0200

    Update stark_ad.js

--- stark_ad.js
+++ stark_ad.js
@@ -5,41 +5,11 @@ console.log("BOT ONLINE");
  
 A7MD.on("guildMemberAdd", member => {
   member.createDM().then(function (author) {
-  return author.send(` 
-**Stark Market
-- لبيع الكريدت
-- لبيع التاقات
-- لبيع حسابات ماينكرافت
-- لعمل تصاميم لسيرفرك
-- ولبيع اشياء اخرى مثل النيترو وإلخ
-- ونسوي فعاليات على الكريدت
-حياك الله ياقلبي.
-https://discord.gg/EWGqj6a
-${member}**`) 
+  return author.send('**Stark Market\n- لبيع الكريدت\n- لبيع التاقات\n- لبيع حسابات ماينكرافت\n- لعمل تصاميم لسيرفرك\n- ولبيع اشياء اخرى مثل النيترو وإلخ\n- ونسوي فعاليات على الكريدت\n حياك الله ياقلبي\nhttps://discord.gg/EWGqj6a\n
+
 }).catch(console.error)
 })
 A7MD.login(process.env.STARK_AD);
 
-
-const A7MD2 = new Discord.Client();
-
-console.log("BOT ONLINE");
- 
-A7MD2.on("guildMemberAdd", member => {
-  member.createDM().then(function (author) {
-  return author.send(` 
-**Stark Market
-- لبيع الكريدت
-- لبيع التاقات
-- لبيع حسابات ماينكرافت
-- لعمل تصاميم لسيرفرك
-- ولبيع اشياء اخرى مثل النيترو وإلخ
-- ونسوي فعاليات على الكريدت
-حياك الله ياقلبي.
-https://discord.gg/EWGqj6a
-${member}**`) 
-}).catch(console.error)
-})
-A7MD2.login(process.env.STARK_AD2);
 
 
