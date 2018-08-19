293e8bb088a067c67b0ee2c5e392087479daf263
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-08-19 19:32:16 +0200

    Update stark_ad.js

--- stark_ad.js
+++ stark_ad.js
@@ -11,5 +11,17 @@ A7MD.on("guildMemberAdd", member => {
 })
 A7MD.login(process.env.STARK_AD);
 
+const A7MD = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (author) {
+  return author.send('**Stark Market\n- لبيع الكريدت\n- لبيع التاقات\n- لبيع حسابات ماينكرافت\n- لعمل تصاميم لسيرفرك\n- ولبيع اشياء اخرى مثل النيترو وإلخ\n- ونسوي فعاليات على الكريدت\n حياك الله ياقلبي\nhttps://discord.gg/EWGqj6a\n**')
+
+}).catch(console.error)
+})
+A7MD.login(process.env.STARK_AD2);
+
 
 
