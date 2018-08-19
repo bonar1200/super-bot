16f9ed7846efbc373c5aadc69591d4948a864ddc
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-08-19 14:41:14 +0200

    Update stark_ad2.js

--- stark_ad2.js
+++ stark_ad2.js
@@ -7,14 +7,144 @@ A7MD.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
 **Stark Market
+
 - لبيع الكريدت
 - لبيع التاقات
 - لعمل تصاميم لسيرفرك
 - ويوجد لدينا بيع حسابات ماينكرافت
 - ونسوي فعاليات على الكريدت
+
 حياك الله ياقلبي.
+
 https://discord.gg/EWGqj6a
+
 ${member}**`) 
 }).catch(console.error)
 })
-A7MD.login(process.env.STARK_AD);
+A7MD.login(process.env.STARK_AD7);
+
+const Discord = require('discord.js');
+const A7MD2 = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD2.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**Stark Market
+
+- لبيع الكريدت
+- لبيع التاقات
+- لعمل تصاميم لسيرفرك
+- ويوجد لدينا بيع حسابات ماينكرافت
+- ونسوي فعاليات على الكريدت
+
+حياك الله ياقلبي.
+
+https://discord.gg/EWGqj6a
+
+${member}**`) 
+}).catch(console.error)
+})
+A7MD2.login(process.env.STARK_AD8);
+
+const Discord = require('discord.js');
+const A7MD3 = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD3.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**Stark Market
+
+- لبيع الكريدت
+- لبيع التاقات
+- لعمل تصاميم لسيرفرك
+- ويوجد لدينا بيع حسابات ماينكرافت
+- ونسوي فعاليات على الكريدت
+
+حياك الله ياقلبي.
+
+https://discord.gg/EWGqj6a
+
+${member}**`) 
+}).catch(console.error)
+})
+A7MD3.login(process.env.STARK_AD9);
+
+const Discord = require('discord.js');
+const A7MD4 = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD4.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**Stark Market
+
+- لبيع الكريدت
+- لبيع التاقات
+- لعمل تصاميم لسيرفرك
+- ويوجد لدينا بيع حسابات ماينكرافت
+- ونسوي فعاليات على الكريدت
+
+حياك الله ياقلبي.
+
+https://discord.gg/EWGqj6a
+
+${member}**`) 
+}).catch(console.error)
+})
+A7MD4.login(process.env.STARK_AD10);
+
+const Discord = require('discord.js');
+const A7MD5 = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD5.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**Stark Market
+
+- لبيع الكريدت
+- لبيع التاقات
+- لعمل تصاميم لسيرفرك
+- ويوجد لدينا بيع حسابات ماينكرافت
+- ونسوي فعاليات على الكريدت
+
+حياك الله ياقلبي.
+
+https://discord.gg/EWGqj6a
+
+${member}**`) 
+}).catch(console.error)
+})
+A7MD5.login(process.env.STARK_AD11);
+
+const Discord = require('discord.js');
+const A7MD6 = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD6.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**Stark Market
+
+- لبيع الكريدت
+- لبيع التاقات
+- لعمل تصاميم لسيرفرك
+- ويوجد لدينا بيع حسابات ماينكرافت
+- ونسوي فعاليات على الكريدت
+
+حياك الله ياقلبي.
+
+https://discord.gg/EWGqj6a
+
+${member}**`) 
+}).catch(console.error)
+})
+A7MD6.login(process.env.STARK_AD12);
+
