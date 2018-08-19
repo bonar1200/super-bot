5e817a576435809e3a0a5f38550c4c3d84993d7e
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-08-19 14:48:20 +0200

    Update order_ad5.js

--- order_ad5.js
+++ order_ad5.js
@@ -15,3 +15,98 @@ A7MD.on("guildMemberAdd", member => {
 }).catch(console.error)
 })
 A7MD.login(process.env.ORDER_TOKEN);
+
+const Discord = require('discord.js');
+const A7MD2 = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD2.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم 
+لو سمحت ممكن تدخل سيرفرى
+                               [ رابط السيرفر ] 
+الدعووة خاصة لك ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+A7MD2.login(process.env.ORDER_TOKEN2);
+
+
+const Discord = require('discord.js');
+const A7MD3 = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD3.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم 
+لو سمحت ممكن تدخل سيرفرى
+                               [ رابط السيرفر ] 
+الدعووة خاصة لك ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+A7MD3.login(process.env.ORDER_TOKEN3);
+
+
+const Discord = require('discord.js');
+const A7MD4 = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD4.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم 
+لو سمحت ممكن تدخل سيرفرى
+                               [ رابط السيرفر ] 
+الدعووة خاصة لك ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+A7MD4.login(process.env.ORDER_TOKEN4);
+
+
+const Discord = require('discord.js');
+const A7MD5 = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD5.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم 
+لو سمحت ممكن تدخل سيرفرى
+                               [ رابط السيرفر ] 
+الدعووة خاصة لك ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+A7MD5.login(process.env.ORDER_TOKEN5);
+
+
+const Discord = require('discord.js');
+const A7MD6 = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD6.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم 
+لو سمحت ممكن تدخل سيرفرى
+                               [ رابط السيرفر ] 
+الدعووة خاصة لك ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+A7MD6.login(process.env.ORDER_TOKEN6);
+
