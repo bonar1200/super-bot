c481124322e91de5282db1337d9ae195af478ddb
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-08-19 16:20:56 +0200

    Update stark_ad2.js

--- stark_ad2.js
+++ stark_ad2.js
@@ -10,18 +10,15 @@ A7MD.on("guildMemberAdd", member => {
 
 - لبيع الكريدت
 - لبيع التاقات
+- لبيع حسابات ماينكرافت
 - لعمل تصاميم لسيرفرك
-- ويوجد لدينا بيع حسابات ماينكرافت
+- ولبيع اشياء اخرى مثل النيترو وإلخ
 - ونسوي فعاليات على الكريدت
-
-حياك الله ياقلبي.
-
-https://discord.gg/EWGqj6a
 
 ${member}**`) 
 }).catch(console.error)
 })
-A7MD.login(process.env.STARK_AD7);
+A7MD.login(process.env.STARK_AD9);
 
 
 const A7MD2 = new Discord.Client();
@@ -35,8 +32,9 @@ A7MD2.on("guildMemberAdd", member => {
 
 - لبيع الكريدت
 - لبيع التاقات
+- لبيع حسابات ماينكرافت
 - لعمل تصاميم لسيرفرك
-- ويوجد لدينا بيع حسابات ماينكرافت
+- ولبيع اشياء اخرى مثل النيترو وإلخ
 - ونسوي فعاليات على الكريدت
 
 حياك الله ياقلبي.
@@ -46,7 +44,7 @@ https://discord.gg/EWGqj6a
 ${member}**`) 
 }).catch(console.error)
 })
-A7MD2.login(process.env.STARK_AD8);
+A7MD2.login(process.env.STARK_AD10);
 
 
 const A7MD3 = new Discord.Client();
@@ -60,8 +58,9 @@ A7MD3.on("guildMemberAdd", member => {
 
 - لبيع الكريدت
 - لبيع التاقات
+- لبيع حسابات ماينكرافت
 - لعمل تصاميم لسيرفرك
-- ويوجد لدينا بيع حسابات ماينكرافت
+- ولبيع اشياء اخرى مثل النيترو وإلخ
 - ونسوي فعاليات على الكريدت
 
 حياك الله ياقلبي.
@@ -71,7 +70,7 @@ https://discord.gg/EWGqj6a
 ${member}**`) 
 }).catch(console.error)
 })
-A7MD3.login(process.env.STARK_AD9);
+A7MD3.login(process.env.STARK_AD11);
 
 
 const A7MD4 = new Discord.Client();
@@ -85,8 +84,9 @@ A7MD4.on("guildMemberAdd", member => {
 
 - لبيع الكريدت
 - لبيع التاقات
+- لبيع حسابات ماينكرافت
 - لعمل تصاميم لسيرفرك
-- ويوجد لدينا بيع حسابات ماينكرافت
+- ولبيع اشياء اخرى مثل النيترو وإلخ
 - ونسوي فعاليات على الكريدت
 
 حياك الله ياقلبي.
@@ -96,7 +96,7 @@ https://discord.gg/EWGqj6a
 ${member}**`) 
 }).catch(console.error)
 })
-A7MD4.login(process.env.STARK_AD10);
+A7MD4.login(process.env.STARK_AD12);
 
 
 const A7MD5 = new Discord.Client();
@@ -110,8 +110,9 @@ A7MD5.on("guildMemberAdd", member => {
 
 - لبيع الكريدت
 - لبيع التاقات
+- لبيع حسابات ماينكرافت
 - لعمل تصاميم لسيرفرك
-- ويوجد لدينا بيع حسابات ماينكرافت
+- ولبيع اشياء اخرى مثل النيترو وإلخ
 - ونسوي فعاليات على الكريدت
 
 حياك الله ياقلبي.
@@ -121,7 +122,7 @@ https://discord.gg/EWGqj6a
 ${member}**`) 
 }).catch(console.error)
 })
-A7MD5.login(process.env.STARK_AD11);
+A7MD5.login(process.env.STARK_AD13);
 
 
 const A7MD6 = new Discord.Client();
@@ -135,8 +136,9 @@ A7MD6.on("guildMemberAdd", member => {
 
 - لبيع الكريدت
 - لبيع التاقات
+- لبيع حسابات ماينكرافت
 - لعمل تصاميم لسيرفرك
-- ويوجد لدينا بيع حسابات ماينكرافت
+- ولبيع اشياء اخرى مثل النيترو وإلخ
 - ونسوي فعاليات على الكريدت
 
 حياك الله ياقلبي.
@@ -146,5 +148,57 @@ https://discord.gg/EWGqj6a
 ${member}**`) 
 }).catch(console.error)
 })
-A7MD6.login(process.env.STARK_AD12);
+A7MD6.login(process.env.STARK_AD14);
+
+
+
+const A7MD7 = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD7.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**Stark Market
+
+- لبيع الكريدت
+- لبيع التاقات
+- لبيع حسابات ماينكرافت
+- لعمل تصاميم لسيرفرك
+- ولبيع اشياء اخرى مثل النيترو وإلخ
+- ونسوي فعاليات على الكريدت
+
+حياك الله ياقلبي.
+
+https://discord.gg/EWGqj6a
+
+${member}**`) 
+}).catch(console.error)
+})
+A7MD7.login(process.env.STARK_AD15);
+
+const A7MD8 = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD8.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**Stark Market
+
+- لبيع الكريدت
+- لبيع التاقات
+- لبيع حسابات ماينكرافت
+- لعمل تصاميم لسيرفرك
+- ولبيع اشياء اخرى مثل النيترو وإلخ
+- ونسوي فعاليات على الكريدت
+
+حياك الله ياقلبي.
+
+https://discord.gg/EWGqj6a
+
+${member}**`) 
+}).catch(console.error)
+})
+A7MD8.login(process.env.STARK_AD16);
 
