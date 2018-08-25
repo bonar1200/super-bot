899f35534287e8d4b7748448c34780cd84b101ef
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-08-25 18:12:18 +0200

    Update discord_anti_spam.js

--- discord_anti_spam.js
+++ discord_anti_spam.js
@@ -7,7 +7,7 @@ anti_spam(bot, {
   warnBuffer: 3, //Maximum amount of messages allowed to send in the interval time before getting warned. 
   maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned. 
   interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned. 
-  warningMessage: "**وقف سبام او راح تتبند**", // Warning message send to the user indicating they are going to fast. 
+  warningMessage: "**سيتم طردك إن لم توقف سبام**", // Warning message send to the user indicating they are going to fast. 
   banMessage: "تم الطرد بسبب السبام", // Ban message, always tags the banned user in front of it. 
   maxDuplicatesWarning: 7, // Maximum amount of duplicate messages a user can send in a timespan before getting warned 
   maxDuplicatesBan: 10 // Maximum amount of duplicate messages a user can send in a timespan before getting banned 
