6df21ad1be7f5d234a6ec49c03249300036e3f91
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-08-30 22:30:57 +0200

    Update discord_anti_spam.js

--- discord_anti_spam.js
+++ discord_anti_spam.js
@@ -5,7 +5,7 @@ var anti_spam = require("discord-anti-spam");
  
 anti_spam(bot, {
   warnBuffer: 4, //Maximum amount of messages allowed to send in the interval time before getting warned. 
-  maxBuffer: 6, // Maximum amount of messages allowed to send in the interval time before getting banned. 
+  maxBuffer: 7, // Maximum amount of messages allowed to send in the interval time before getting banned. 
   interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned. 
   warningMessage: "**سيتم طردك إن لم توقف سبام**", // Warning message send to the user indicating they are going to fast. 
   banMessage: "تم الطرد بسبب السبام", // Ban message, always tags the banned user in front of it. 
@@ -20,7 +20,7 @@ const bot1 = new Discord.Client();
  var anti_spam1 = require("discord-anti-spam");
 anti_spam1(bot1, {
   warnBuffer: 4, //Maximum amount of messages allowed to send in the interval time before getting warned. 
-  maxBuffer: 6, // Maximum amount of messages allowed to send in the interval time before getting banned. 
+  maxBuffer: 7, // Maximum amount of messages allowed to send in the interval time before getting banned. 
   interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned. 
   warningMessage: "**سيتم طردك إن لم توقف سبام**", // Warning message send to the user indicating they are going to fast. 
   banMessage: "تم الطرد بسبب السبام", // Ban message, always tags the banned user in front of it. 
