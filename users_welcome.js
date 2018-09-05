a4722f13273990c43cc6ad1fbac9b6d8ae39cc16
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-09-05 04:37:12 +0200

    Update users_welcome.js

--- users_welcome.js
+++ users_welcome.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client();
 
 bot.on("guildMemberAdd", member => {
   let guild = member.guild;
-  bot.channels.get("354687515556315137").sendMessage(" :star: **__Welcome To Free Time Server __** :star: ")
+  bot.channels.get("486649501612769291").sendMessage(" :star: **__Welcome To Free Time Server __** :star: ")
   });
   
 bot.login(process.env.WELCOME);
@@ -12,7 +12,7 @@ const bot2 = new Discord.Client();
 
 bot2.on("guildMemberAdd", member => {
   let guild = member.guild;
-  bot2.channels.get("354687515556315137").sendMessage(" :full_moon_with_face: **Welcome To Free Time Server** :full_moon_with_face: ")
+  bot2.channels.get("486649501612769291").sendMessage(" :full_moon_with_face: **Welcome To Free Time Server** :full_moon_with_face: ")
   });
   
 bot2.login(process.env.WELCOME2);
@@ -21,7 +21,7 @@ const bot3 = new Discord.Client();
 
 bot3.on("guildMemberAdd", member => {
   let guild = member.guild;
-  bot3.channels.get("354687515556315137").sendMessage("`Welcome To Free Time Server` :black_heart: ")
-    bot3.channels.get("354687515556315137").sendMessage("ولكم")
+  bot3.channels.get("486649501612769291").sendMessage("`Welcome To Free Time Server` :black_heart: ")
+    bot3.channels.get("486649501612769291").sendMessage("ولكم")
   });
   bo3.login(process.env.WELCOME3);
