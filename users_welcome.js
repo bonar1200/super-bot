713dea7e24c9eaca7ec06814a54b2c9993bb392a
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-09-05 04:35:55 +0200

    Create users_welcome.js

---
+++ users_welcome.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+bot.on("guildMemberAdd", member => {
+  let guild = member.guild;
+  bot.channels.get("354687515556315137").sendMessage(" :star: **__Welcome To Free Time Server __** :star: ")
+  });
+  
+bot.login(process.env.WELCOME);
+
+const bot2 = new Discord.Client();
+
+bot2.on("guildMemberAdd", member => {
+  let guild = member.guild;
+  bot2.channels.get("354687515556315137").sendMessage(" :full_moon_with_face: **Welcome To Free Time Server** :full_moon_with_face: ")
+  });
+  
+bot2.login(process.env.WELCOME2);
+
+const bot3 = new Discord.Client();
+
+bot3.on("guildMemberAdd", member => {
+  let guild = member.guild;
+  bot3.channels.get("354687515556315137").sendMessage("`Welcome To Free Time Server` :black_heart: ")
+    bot3.channels.get("354687515556315137").sendMessage("ولكم")
+  });
+  bo3.login(process.env.WELCOME3);
