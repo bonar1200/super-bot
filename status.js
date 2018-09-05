6df37931f40a589804f243b59c4ce8fa91750ecb
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-09-06 01:35:36 +0200

    Update status.js

--- status.js
+++ status.js
@@ -5,4 +5,46 @@ client.on('ready',  () => {
     client.user.setGame("Free Time Music", "https://www.twitch.tv/FreeTime");
 });
 
-client.login(process.env.STATUS);
+client.login(process.env.PLAY1);
+
+const client2 = new Discord.Client();
+
+client2.on('ready',  () => {
+    client2.user.setGame("Free Time Music", "https://www.twitch.tv/FreeTime");
+});
+
+client2.login(process.env.PLAY2);
+
+const client3 = new Discord.Client();
+
+client3.on('ready',  () => {
+    client3.user.setGame("Free Time Music", "https://www.twitch.tv/FreeTime");
+});
+
+client3.login(process.env.PLAY3);
+
+const client4 = new Discord.Client();
+
+client4.on('ready',  () => {
+    client4.user.setGame("Free Time Music", "https://www.twitch.tv/FreeTime");
+});
+
+client4.login(process.env.PLAY4);
+
+const client5 = new Discord.Client();
+
+client5.on('ready',  () => {
+    client5.user.setGame("Free Time Music", "https://www.twitch.tv/FreeTime");
+});
+
+client5.login(process.env.PLAY5);
+
+const client6 = new Discord.Client();
+
+client6.on('ready',  () => {
+    client6.user.setGame("Free Time Music", "https://www.twitch.tv/FreeTime");
+});
+
+client6.login(process.env.PLAY6);
+
+
