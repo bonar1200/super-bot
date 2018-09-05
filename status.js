b20b698eeb641e4abfcd61665dd9d19732873ae9
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-09-05 22:37:27 +0200

    Create status.js

---
+++ status.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready',  () => {
+    client.user.setGame("Free Time Music", "https://www.twitch.tv/FreeTime");
+});
+
+client.login(process.env.status);
