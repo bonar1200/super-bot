f8ca37f3dc2665a34de6453426d83edc1c52ceba
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-11-21 14:13:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-client.user.setActivity(",",{type: 'PLAYING'})
+client.user.setActivity("6$ = Million Credits ابيع كريدت",{type: 'PLAYING'})
 	      var interval = setInterval (function () {
 client.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
 		  }, 120000);
