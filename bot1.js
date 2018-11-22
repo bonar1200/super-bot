98bbd6cd196e580d684decfa344966c2fb07e15f
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-11-22 20:08:28 +0200

    Update bot1.js

--- bot1.js
+++ bot1.js
@@ -4,19 +4,7 @@ const client = new Discord.Client();
 client.on("ready", () => {
      client.user.setActivity("Your Ass",{type: 'WATCHING'})
 
-	      var interval = setInterval (function () {
-client.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
-		  }, 120000);
-  
-	      var interval1 = setInterval (function () {
-  client.channels.get('451866194559893504').send('#daily');
-  client.channels.get('451866194559893504').send('#rep <@215251553538408458>');  
-		  }, 86800000);
-  
-	      var interval2 = setInterval (function () {
-client.channels.get('451866194559893504').send('t!daily <@215251553538408458>');
-client.channels.get('451866194559893504').send('t!rep <@215251553538408458>');
-		  }, 61360000);
+
 });
 client.login(process.env.BOT_TOKENN);
 
@@ -25,18 +13,7 @@ const client1 = new Discord.Client();
 
 client1.on("ready", () => {
 
-	      var interval = setInterval (function () {
-client1.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
-		  }, 120000);
-  
-  	      var interval1 = setInterval (function () {
-  client1.channels.get('451866194559893504').send('#daily');
-  client1.channels.get('451866194559893504').send('#rep <@215251553538408458>');  
-		  }, 86800000);
-	      var interval2 = setInterval (function () {
-client1.channels.get('451866194559893504').send('t!daily <@215251553538408458>');
-client1.channels.get('451866194559893504').send('t!rep <@215251553538408458>');
-		  }, 61360000);
+
 });
 client1.login(process.env.STARK_BANK);
 // //
@@ -45,18 +22,7 @@ const client2 = new Discord.Client();
 
 client2.on("ready", () => {
 
-	      var interval = setInterval (function () {
-client2.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
-		  }, 120000);
-  
-  	      var interval1 = setInterval (function () {
-  client2.channels.get('451866194559893504').send('#daily');
-  client2.channels.get('451866194559893504').send('#rep <@215251553538408458>');  
-		  }, 86800000);
-	      var interval2 = setInterval (function () {
-client2.channels.get('451866194559893504').send('t!daily <@215251553538408458>');
-client2.channels.get('451866194559893504').send('t!rep <@215251553538408458>');
-		  }, 61360000);
+
 });
 client2.login(process.env.STARK_TOKEN1);
 // //
@@ -65,18 +31,8 @@ const client3 = new Discord.Client();
 
 client3.on("ready", () => {
 
-	      var interval = setInterval (function () {
-client3.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
-		  }, 120000);
-  
-  	      var interval1 = setInterval (function () {
-  client3.channels.get('451866194559893504').send('#daily');
-  client3.channels.get('451866194559893504').send('#rep <@215251553538408458>');  
-		  }, 86800000);
-	      var interval2 = setInterval (function () {
-client3.channels.get('451866194559893504').send('t!daily <@215251553538408458>');
-client3.channels.get('451866194559893504').send('t!rep <@215251553538408458>');
-		  }, 61360000);
+
+
 });
 client3.login(process.env.STARK_TOKEN2);
 // //
@@ -85,18 +41,7 @@ const client4 = new Discord.Client();
 
 client4.on("ready", () => {
 client4.user.setActivity("Minecraft",{type: 'PLAYING'})
-	      var interval = setInterval (function () {
-client4.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
-		  }, 120000);
-  
-  	      var interval1 = setInterval (function () {
-  client4.channels.get('451866194559893504').send('#daily');
-  client4.channels.get('451866194559893504').send('#rep <@215251553538408458>');  
-		  }, 86800000);
-	      var interval2 = setInterval (function () {
-client4.channels.get('451866194559893504').send('t!daily <@215251553538408458>');
-client4.channels.get('451866194559893504').send('t!rep <@215251553538408458>');
-		  }, 61360000);
+
 });
 client4.login(process.env.LOUIS);
 
