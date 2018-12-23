88c2ec8bd7ba51477d7d87030a834c85fff27d77
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2018-12-23 19:26:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on("ready", () => {
-client.user.setActivity("6$ = Million Credits ابيع كريدت",{type: 'PLAYING'})
 
-});
 client.login(process.env.BOT_TOKEN);
 
 // //
