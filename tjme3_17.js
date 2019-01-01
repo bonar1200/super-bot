72cba34225a430060e99306f28d65cba0006657f
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2019-01-01 14:35:00 +0200

    Update tjme3_17.js

--- tjme3_17.js
+++ tjme3_17.js
@@ -4,17 +4,17 @@ const client = new Discord.Client();
 client.on("ready", () => {
 
           var interval = setInterval (function () {
-client.channels.get('451866194559893504').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
+client.channels.get('529636546010152981').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');
           }, 120000);
   
           var interval1 = setInterval (function () {
-  client.channels.get('451866194559893504').send('#daily');
-  client.channels.get('451866194559893504').send('#rep <@215251553538408458>');  
+  client.channels.get('529636546010152981').send('#daily');
+  client.channels.get('529636546010152981').send('#rep <@215251553538408458>');  
           }, 86800000);
   
           var interval2 = setInterval (function () {
-client.channels.get('451866194559893504').send('t!daily <@215251553538408458>');
-client.channels.get('451866194559893504').send('t!rep <@215251553538408458>');
+client.channels.get('529636546010152981').send('t!daily <@215251553538408458>');
+client.channels.get('529636546010152981').send('t!rep <@215251553538408458>');
           }, 61360000);
 });
 
