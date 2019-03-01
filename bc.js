5991f5a028121c8b0f5993781871c0644836f542
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2019-03-01 22:29:01 +0200

    Update bc.js

--- bc.js
+++ bc.js
@@ -133,4 +133,14 @@ if(message.content.startsWith(prefix + "setnothing")){
  message.channel.send(`Done!`)
  }
 });
+
+client.on("guildMemberAdd", async member => {
+  let moment2 = require('moment-duration-format'),
+      moment = require("moment"),
+      date = moment.duration(new Date() - member.user.createdAt).format("d");
+
+  if(date < 10) {
+    member.ban("Member account age is lower than 10 days.")
+  }
+});
 bot.login(process.env.BC)
