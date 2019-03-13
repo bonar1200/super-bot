3606220d2f0445fa2e42e87c6af61b67ceda16d8
bonar1200 <40079807+bonar1200@users.noreply.github.com>
2019-03-13 21:54:22 +0200

    Update brc1.js

--- brc1.js
+++ brc1.js
@@ -6,7 +6,7 @@ bot.on(`ready`, () => {
   });
   bot.on("message", async message => {
 
-  let prefix = `$`
+  let prefix = `!`
   let messageArray = message.content.split(" ");
   let msg = message;
   let cmd = messageArray[0];
